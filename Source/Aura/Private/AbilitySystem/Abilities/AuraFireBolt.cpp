17f9a0e3521a25e96bf2bd96849a3334496dab11
Jakub <kubarak326@gmail.com>
2024-02-04 20:33:26 +0100

    Deselecting globe

--- Source/Aura/Private/AbilitySystem/Abilities/AuraFireBolt.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraFireBolt.cpp
@@ -37,7 +37,7 @@ FString UAuraFireBolt::GetDescription(int32 Level)
 FString UAuraFireBolt::GetNextLevel(int32 Level)
 {
 	const int32 Damage = GetDamageByDamageType(Level, FAuraGameplayTags::Get().Damage_Fire);
-	const float ManaCost =  FMath::Abs(GetManaCost(Level));
+	const float ManaCost = FMath::Abs(GetManaCost(Level));
 	const float Cooldown = GetCooldown(Level);
 	return FString::Printf(TEXT(
 	"<Title>NEXT LEVEL</>\n\n"
