444e6f36ab9944c072eed70ff15fc13674e5ea71
Jakub <kubarak326@gmail.com>
2024-02-04 18:36:26 +0100

    Spell Descriptions

--- Source/Aura/Private/AbilitySystem/Abilities/AuraGameplayAbility.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraGameplayAbility.cpp
@@ -3,3 +3,17 @@
 
 #include "AbilitySystem/Abilities/AuraGameplayAbility.h"
 
+FString UAuraGameplayAbility::GetDescription(int32 Level)
+{
+	return FString::Printf(TEXT("<Default>%s,</><Level>%d</>"), L"Default Ability Name - LoremIpsum LoremIpsum LoremIpsum LoremIpsum LoremIpsum LoremIpsum LoremIpsumLoremIpsum", Level);
+}
+
+FString UAuraGameplayAbility::GetNextLevel(int32 Level)
+{
+	return FString::Printf(TEXT("<Default>Next Level: </><Level>%d</> \n<Default>Deals much more </><Damage>damage</>"),Level);
+}
+
+FString UAuraGameplayAbility::GetLockedDescription(int32 Level)
+{
+	return FString::Printf(TEXT("<Default>Spell Locked Until Level: %d</>"),Level);
+}
