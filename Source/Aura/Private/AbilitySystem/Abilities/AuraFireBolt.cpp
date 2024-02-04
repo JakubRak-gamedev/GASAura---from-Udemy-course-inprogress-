48fe74ceae8a1db344716f6e38f7649fa3bfde4e
Jakub <kubarak326@gmail.com>
2024-02-04 20:17:10 +0100

    Cost and Cooldown in Spell Description

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraFireBolt.cpp
@@ -0,0 +1,50 @@
+// Jakub Rak
+
+
+#include "AbilitySystem/Abilities/AuraFireBolt.h"
+
+#include "Interaction/CombatInterface.h"
+#include "Aura/Public/AuraGameplayTags.h"
+
+
+FString UAuraFireBolt::GetDescription(int32 Level)
+{
+	
+	const int32 Damage = GetDamageByDamageType(Level, FAuraGameplayTags::Get().Damage_Fire);
+	const float ManaCost = FMath::Abs(GetManaCost(Level));
+	const float Cooldown = GetCooldown(Level);
+	if(Level > 1)
+	{
+		return FString::Printf(TEXT(
+		"<Title>FIRE BOLT</>\n\n"
+		"<Default>Level: %d</>\n"
+		"<Default>ManaCost: </><ManaCost>%.1f</>\n"
+		"<Default>Cooldown: </><Cooldown>%.1f</>\n\n"
+		"<Default>Launches %d bolts of fire, "
+		"exploding on impact and dealing: "
+		"</><Damage>%d</><Default> fire damage with chance to burn</>\n\n"),Level, ManaCost, Cooldown, Level, Damage);
+	}
+	return FString::Printf(TEXT(
+	"<Title>FIRE BOLT</>\n\n"
+	"<Default>Level: %d</>\n"
+	"<Default>ManaCost: </><ManaCost>%.1f</>\n"
+	"<Default>Cooldown: </><Cooldown>%.1f</>\n\n"
+	"<Default>Launch a bolt of fire, "
+	"exploding on impact and dealing: "
+	"</><Damage>%d</><Default> fire damage with chance to burn</>\n\n") ,Level, ManaCost, Cooldown, Damage);
+}
+
+FString UAuraFireBolt::GetNextLevel(int32 Level)
+{
+	const int32 Damage = GetDamageByDamageType(Level, FAuraGameplayTags::Get().Damage_Fire);
+	const float ManaCost =  FMath::Abs(GetManaCost(Level));
+	const float Cooldown = GetCooldown(Level);
+	return FString::Printf(TEXT(
+	"<Title>NEXT LEVEL</>\n\n"
+	"<Default>Level: %d</>\n"
+	"<Default>ManaCost: </><ManaCost>%.1f</>\n"
+	"<Default>Cooldown: </><Cooldown>%.1f</>\n\n"
+	"<Default>Launches %d bolts of fire, "	
+	"exploding on impact and dealing: "
+	"</><Damage>%d</><Default> fire damage with chance to burn</>\n\n"),Level,ManaCost,Cooldown,Level,Damage);
+}
--- Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -9,23 +9,6 @@
 #include "Interaction/CombatInterface.h"
 #include "Aura/Public/AuraGameplayTags.h"
 
-FString UAuraProjectileSpell::GetDescription(int32 Level)
-{
-	
-	const int32 Damage = DamageTypes[FAuraGameplayTags::Get().Damage_Fire].GetValueAtLevel(Level);
-	if(Level > 1)
-	{
-		return FString::Printf(TEXT("<Title>FIRE BOLT</>\n\n<Default>Launches %d bolts of fire, exploding on impact and dealing: </><Damage>%d</><Default> fire damage with chance to burn</>\n\n<Default>Level: %d</>"),Level,Damage,Level);
-	}
-	return FString::Printf(TEXT("<Title>FIRE BOLT</>\n\n<Default>Launch bolt of fire, exploding on impact and dealing: </><Damage>%d</><Default> fire damage with chance to burn</>\n\n<Default>Level: %d</>"),Damage,Level);
-}
-
-FString UAuraProjectileSpell::GetNextLevel(int32 Level)
-{
-	const int32 Damage = DamageTypes[FAuraGameplayTags::Get().Damage_Fire].GetValueAtLevel(Level+1);
-	return FString::Printf(TEXT("<Title>NEXT LEVEL</>\n\n<Default>Launches %d bolts of fire, exploding on impact and dealing: </><Damage>%d</><Default> fire damage with chance to burn</>\n\n<Default>Level: %d</>"),Level,Damage,Level);
-}
-
 void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
                                            const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
                                            const FGameplayEventData* TriggerEventData)
