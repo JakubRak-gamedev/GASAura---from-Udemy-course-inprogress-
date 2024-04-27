c1a35a19adf59de0e15d97d831edc873b23d6910
Jakub <kubarak326@gmail.com>
2024-04-27 12:54:45 +0200

    niagara passive abilities wszystko

--- Source/Aura/Public/AbilitySystem/AuraAbilitySystemComponent.h
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemComponent.h
@@ -12,6 +12,7 @@ DECLARE_DELEGATE_OneParam(FForEachAbility, const FGameplayAbilitySpec&);
 DECLARE_MULTICAST_DELEGATE_ThreeParams(FAbilityStatusChanged, const FGameplayTag& /*AbilityTag*/,const FGameplayTag& /*StatusTag*/, int32 /*AbilityLevel*/);
 DECLARE_MULTICAST_DELEGATE_FourParams(FAbilityEquipped,const FGameplayTag& /*AbilityTag*/,const FGameplayTag& /*StatusTag*/, const FGameplayTag& /*Slot*/,const FGameplayTag& /*PrevSlot*/);
 DECLARE_MULTICAST_DELEGATE_OneParam(FDeactivatePassiveAbility, const FGameplayTag& /*Ability Tag*/);
+DECLARE_MULTICAST_DELEGATE_TwoParams(FActivatePassiveEffect, const FGameplayTag& /*Ability Tag*/, bool /*bActivate*/);
 /**
  * 
  */
@@ -27,6 +28,7 @@ public:
 	FAbilityStatusChanged AbilityStatusChanged;
 	FAbilityEquipped AbilityEquipped;
 	FDeactivatePassiveAbility DeactivatePassiveAbility;
+	FActivatePassiveEffect ActivatePassiveEffect;
 
 	void AddCharacterAbilities(const TArray<TSubclassOf<UGameplayAbility>>& StartupAbilities);
 	void AddCharacterPassiveAbilities(const TArray<TSubclassOf<UGameplayAbility>>& StartupPassiveAbilities);
@@ -48,6 +50,9 @@ public:
 	FGameplayAbilitySpec* GetSpecWithSlot(const FGameplayTag& Slot);
 	bool IsPassiveAbility(const FGameplayAbilitySpec& Spec) const;
 	static void AssignSlotToAbility(FGameplayAbilitySpec& Spec, const FGameplayTag& Slot);
+
+	UFUNCTION(NetMulticast, Unreliable)
+	void MulticastActivatePassiveEffect(const FGameplayTag& AbilityTag, bool bActivate);
 
 	FGameplayAbilitySpec* GetSpecFromAbilityTag(const FGameplayTag& AbilityTag);
 
--- Source/Aura/Public/AbilitySystem/Passive/PassiveNiagaraComponent.h
+++ Source/Aura/Public/AbilitySystem/Passive/PassiveNiagaraComponent.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "NiagaraComponent.h"
+#include "GameplayTagContainer.h"
 #include "PassiveNiagaraComponent.generated.h"
 
 /**
@@ -14,4 +15,14 @@ class AURA_API UPassiveNiagaraComponent : public UNiagaraComponent
 {
 	GENERATED_BODY()
 	
+public:
+	UPassiveNiagaraComponent();
+
+	UPROPERTY(EditDefaultsOnly)
+	FGameplayTag PassiveSpellTag;
+
+protected:
+	virtual void BeginPlay() override;
+
+	void OnPassiveActivate(const FGameplayTag& AbilityTag, bool bActivate);
 };
